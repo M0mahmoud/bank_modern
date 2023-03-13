372bf5bc7af57b76f706c470c67f9857d951a705
M0mahmoud <houdmohamed85@gmail.com>
2023-03-14 01:32:42 +0200

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import styles from "./style";
 
@@ -6,11 +7,15 @@ function App() {
     <>
       <div className="bg-primary w-full overflow-hidden">
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-          <div className={`${styles.boxWidth}`}><Navbar/></div>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
         </div>
 
         <div className={`bg-primary ${styles.flexStart}`}>
-          <div className={`${styles.boxWidth}`}>Hero</div>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
         </div>
 
         <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
